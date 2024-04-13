1a3ca68047585bd578d85907bd9ec338206c2004
vikas0497 <vikasgupta0497@gmail.com>
2024-04-13 13:10:16 +0530

    Created api client to call weather service api

---
+++ weather_forecast/weather_api/api_client.py
@@ -0,0 +1,5 @@
+class API_CLIENT:
+
+    def __init__(self) -> None:
+        self.base_one_call = "https://api.openweathermap.org/data/3.0/onecall"
+        self.base_time_stamp_call = "https://api.openweathermap.org/data/3.0/onecall/timemachine"
